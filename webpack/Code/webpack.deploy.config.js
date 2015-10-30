ae6d7c97461a3aa6a0851a6580b8fee1e6297d38
huangjiangtao <huangjiangtao@renrendai.com>
2015-10-30 17:01:58 +0800

    use html-webpack-plugin to inject js/css

--- webpack/Code/webpack.deploy.config.js
+++ webpack/Code/webpack.deploy.config.js
@@ -3,31 +3,41 @@ var webpack           = require('webpack');
 var node_modules      = path.resolve(__dirname, 'node_modules');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-
-var config = {
+var DEPLOY            = process.env.webpack_deploy;
+DEPLOY = false;
+var webpackConfig = {
 	entry: {
-		app: path.resolve(__dirname, 'app/main.js'),
-		mobile: path.resolve(__dirname, 'app/mobile.js'),
+		app:     path.resolve(__dirname, 'app/main.js'),
+		mobile:  path.resolve(__dirname, 'app/mobile.js'),
 		vendors: ['react']
 	},
 	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: '[name].js'
+		path:       path.resolve(__dirname, 'dist'),
+		filename:   '[name].js?v=[chunkhash]',
+		publicPath: !DEPLOY ? 'dist' : 'http://www.imjiangtao.com'
 	},
+	plugins: [
+    	new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
+		new ExtractTextPlugin('styles.css'),
+    	new HtmlWebpackPlugin(),
+    	new HtmlWebpackPlugin({
+			template: 'app/custom.html',
+			filename: 'custom.html',
+			chunks:   ['mobile'],
+			inject:   'body'
+		})
+  	],
 	module: {
 		loaders: [{
-			test: /\.jsx?$/,
+			test:    /\.jsx?$/,
 			exclude: [node_modules],
-			loader: 'babel'
+			loader:  'babel'
 		}, {
-			test: /\.css$/,
-			loader:  ExtractTextPlugin.extract("style-loader", "css-loader")
+			test:   /\.css$/,
+			loader: ExtractTextPlugin.extract("style-loader", "css-loader")
 		}, {
 			test: /\.s(a|c)ss$/,
 			loader: 'style!css!sass',
-			loader:  ExtractTextPlugin.extract("style-loader", "css-loader")
-
 		}, {
 			test: /\.less$/,
 			loader: 'style!css!less',
@@ -38,12 +48,7 @@ var config = {
 			test: /\.woff$/,
 			loader: 'url?limit=10240'
 		}]
-	},
-	plugins: [
-    	new webpack.optimize.CommonsChunkPlugin('vendors', 'vendors.js'),
-		new ExtractTextPlugin('styles.css'),
-    	new HtmlWebpackPlugin()
-  	]
+	}
 };
 
-module.exports = config;
+module.exports = webpackConfig;
