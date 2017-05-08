a7d9c4e091f3a4d0b9a55e139edb20770c1d6832
Jerret321 <321jiangtao@gmail.com>
2017-05-08 15:05:00 +0800

    delete extra

--- Android-Update/ppt/android-update.md
+++
@@ -1,3 +0,0 @@
-title: Android apk管理和自动更新
-speaker: 江涛
-url: https://github.com/jiangtao/Android-Update
--- Async/Code/src/babel_base.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by jiangtao on 1/30/16.
- */
-
-import babelPolyfill from 'babel-polyfill';
-import babelRegister from 'babel-core/register';
